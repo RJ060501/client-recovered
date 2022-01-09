a492eefe3224fea09be0160dc9365c8867ba7f43
RJ060501 <rjrussell605@gmail.com>
2022-01-09 13:42:36 -0700

    Need to figure out routing and props

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,9 @@ import StreamEdit from './streams/StreamEdit';
 import StreamCreate from './streams/StreamCreate';
 import StreamShow from './streams/StreamShow';
 import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
+// import StreamList from './streams/StreamList';
 import Header from './Header';
+import Home from "./home/Home";
 
 
 const App = () => {
@@ -15,13 +16,14 @@ const App = () => {
         <div>
           <Header />
           <Routes>
-            <Route path='/' exact component={StreamList} />
+            <Route path='/' exact component={Home} />
             <Route path='/streams/new' exact component={StreamCreate} />
             <Route path='/streams/edit' exact component={StreamEdit} />
             <Route path='/streams/delete' exact component={StreamDelete} />
             <Route path='/streams/show' exact component={StreamShow} />
           </Routes>
         </div>
+        <Home />
       </BrowserRouter>
     </div>
   );
