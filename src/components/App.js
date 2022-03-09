6553fd87b611a18dd4380f981330c31738a9fc4d
RJ060501 <rjrussell605@gmail.com>
2022-03-09 14:45:33 -0700

    start_url set to / in manifest.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StreamEdit from './streams/StreamEdit';
-import StreamCreate from './streams/StreamCreate';
-import StreamShow from './streams/StreamShow';
-import StreamDelete from './streams/StreamDelete';
+// import StreamEdit from './streams/StreamEdit';
+// import StreamCreate from './streams/StreamCreate';
+// import StreamShow from './streams/StreamShow';
+// import StreamDelete from './streams/StreamDelete';
 // import StreamList from './streams/StreamList';
 import Header from './Header';
 import Home from "./home/Home";
@@ -17,10 +17,10 @@ const App = () => {
           <Header />
           <Routes>
             <Route path='/' exact component={Home} />
-            <Route path='/streams/new' exact component={StreamCreate} />
+            {/* <Route path='/streams/new' exact component={StreamCreate} />
             <Route path='/streams/edit' exact component={StreamEdit} />
             <Route path='/streams/delete' exact component={StreamDelete} />
-            <Route path='/streams/show' exact component={StreamShow} />
+            <Route path='/streams/show' exact component={StreamShow} /> */}
           </Routes>
         </div>
         <Home />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 
 import App from "./components/App";
 import reducers from "./reducers";
